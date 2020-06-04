7001f24fadc88f1f63c46906aeb4617b22b8fc1a
Sam Windell <info@frozenplain.com>
2020-06-05 00:21:29 +0100

    Fix bug with pitch detector not recognising interleaved channels

--- code/signet/subcommands/pitch_detector/pitch_detector.cpp
+++ code/signet/subcommands/pitch_detector/pitch_detector.cpp
@@ -14,11 +14,18 @@ CLI::App *PitchDetector::CreateSubcommandCLI(CLI::App &app) {
 bool PitchDetector::Process(AudioFile &input) {
     if (!input.interleaved_samples.size()) return false;
 
+    // TODO: do this for all channels and use the average pitch
+    std::vector<double> channel_buffer;
+    channel_buffer.reserve(input.NumFrames());
+    for (size_t frame = 0; frame < input.NumFrames(); ++frame) {
+        channel_buffer.push_back(input.interleaved_samples[frame * input.num_channels + 0]);
+    }
+
     dywapitchtracker pitch_tracker;
     dywapitch_inittracking(&pitch_tracker);
 
-    auto detected_pitch = dywapitch_computepitch(&pitch_tracker, input.interleaved_samples.data(), 0,
-                                                 (int)input.interleaved_samples.size());
+    auto detected_pitch =
+        dywapitch_computepitch(&pitch_tracker, channel_buffer.data(), 0, (int)channel_buffer.size());
     if (input.sample_rate != 44100) {
         detected_pitch *= static_cast<double>(input.sample_rate) / 44100.0;
     }
@@ -27,7 +34,8 @@ bool PitchDetector::Process(AudioFile &input) {
         const auto closest_musical_note = FindClosestMidiPitch(detected_pitch);
         MessageWithNewLine("Pitch-Dectector", "Detected a pitch of ", detected_pitch,
                            "Hz, the closest note is ", closest_musical_note.name, " (MIDI number ",
-                           closest_musical_note.midi_note, ")");
+                           closest_musical_note.midi_note, "), which has a pitch of ",
+                           closest_musical_note.pitch);
     } else {
         MessageWithNewLine("Pitch-Dectector", "No pitch could be found");
     }

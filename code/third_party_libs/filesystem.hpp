5b6fe3d0f9a7ebd507d8278bf3ccf31ac1da5e49
Sam Windell <info@frozenplain.com>
2020-06-15 13:06:19 +0100

    Enable std::filesystem only on Windows - I think other platforms are still to inconsistent on how they support it

--- code/third_party_libs/filesystem.hpp
+++ code/third_party_libs/filesystem.hpp
@@ -7,7 +7,7 @@
 #endif
 #endif
 
-#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include) && !defined(__APPLE__)
+#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include) && defined(_WIN32)
 #if __has_include(<filesystem>)
 #define GHC_USE_STD_FS
 #include <filesystem>

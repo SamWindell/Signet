98d7c0fcd9e030886a41346b6de477d69181061e
Sam Windell <info@frozenplain.com>
2020-06-18 10:20:53 +0100

    Add --silent flag to tests so that it can be set from the command line

--- code/tests/tests_main.cpp
+++ code/tests/tests_main.cpp
@@ -3,7 +3,15 @@
 
 #include "common.h"
 
+static void SetSilent(int argc, char **argv) {
+    for (int i = 0; i < argc; ++i) {
+        if (strcmp(argv[i], "--silent") == 0) {
+            SetMessagesEnabled(false);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
-    SetMessagesEnabled(false);
+    SetSilent(argc, argv);
     return doctest::Context(argc, argv).run();
 }

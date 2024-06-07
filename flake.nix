6633bc3dd57286f34b336bbe2e531f0298afb43a
Sam Windell <info@frozenplain.com>
2024-06-07 19:07:24 +0100

    Update Nix version and help clangd find the database

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 

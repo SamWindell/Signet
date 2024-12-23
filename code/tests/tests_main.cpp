e6e346a2ec70a253c25322bc26ee0125ff7a2e56
Sam Windell <info@frozenplain.com>
2024-12-23 11:42:36 +0000

    Change CWD when running tests program so output files are in build/

--- code/tests/tests_main.cpp
+++ code/tests/tests_main.cpp
@@ -2,6 +2,7 @@
 #include "doctest.hpp"
 
 #include "common.h"
+#include "tests_config.h"
 
 static void HandleTestArgs(int argc, char **argv) {
     for (int i = 0; i < argc; ++i) {
@@ -13,6 +14,7 @@ static void HandleTestArgs(int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
+    fs::current_path(BUILD_DIRECTORY);
     HandleTestArgs(argc, argv);
     return doctest::Context(argc, argv).run();
 }

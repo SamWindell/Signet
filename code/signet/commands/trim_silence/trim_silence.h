2e9695059a2754d4fcd65d95ab29e08fcc94087f
Sam Windell <info@frozenplain.com>
2025-04-09 13:19:22 +0100

    fix name

--- code/signet/commands/trim_silence/trim_silence.h
+++ code/signet/commands/trim_silence/trim_silence.h
@@ -7,7 +7,7 @@ class TrimSilenceCommand final : public Command {
   public:
     CLI::App *CreateCommandCLI(CLI::App &app) override;
     void ProcessFiles(AudioFiles &files) override;
-    std::string GetName() const override { return "RemoveSilence"; }
+    std::string GetName() const override { return "TrimSilence"; }
 
   private:
     std::pair<usize, usize> GetLoudRegion(EditTrackedAudioFile &f) const;

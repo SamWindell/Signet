d896f5e34e5b785eb8ca26941226973d5592b02d
Sam Windell <info@frozenplain.com>
2025-12-16 17:06:04 +0000

    update nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
